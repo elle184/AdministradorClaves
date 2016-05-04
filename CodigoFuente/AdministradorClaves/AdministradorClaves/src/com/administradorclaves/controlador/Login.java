aea3e2b675ee0522eaa4bddc8416c81dcc20dfe9
Adrian Becerra <shogunato184@gmail.com>
2016-05-04 15:14:13 -0500

    Mejroas en el administrador de claves
    Se crea el formulario de inicio de sesión y de registro y se agrega la
    hoja de estilos de la aplicación.

--- CodigoFuente/AdministradorClaves/AdministradorClaves/src/com/administradorclaves/controlador/Login.java
+++ CodigoFuente/AdministradorClaves/AdministradorClaves/src/com/administradorclaves/controlador/Login.java
@@ -22,8 +22,30 @@ public class Login implements Serializable {
 	private static final long serialVersionUID = 1L;
 	public static final String MANAGED_BEAN_NAME = "login";
 	private String saludo = "Bienvenido al administrador de claves";
+	private String nomUsuario;
+	private String clave;
+	
+	public String iniciarSesion() {
+		return null;
+	}
 	
 	public String getSaludo() {
 		return this.saludo;
+	}
+
+	public String getNomUsuario() {
+		return nomUsuario;
+	}
+
+	public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
 	}
 }
